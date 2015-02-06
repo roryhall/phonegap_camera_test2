611c4be78edf6c5f5b2eddc8a9b1ee68e7c2a799
Rory Hall <rory@ubuntu.(none)>
2015-02-06 21:07:01 +1000

    add

---
+++ www/assets/js/index.js
@@ -0,0 +1,49 @@
+//-----------------------------------------------------
+// Test 2a Assignment
+
+// Not the best settings as it crops the photo somewhat but there seems to be alot of documented bugs around the samsung phones and cordova to fit the 			   16 megapixel images into memory for some reason.
+
+//-----------------------------------------------------
+
+// Camera Function
+
+
+
+	 function onSuccess(el, source){
+	    if (!navigator.camera){
+	        alert('Camera API not supported', 'Error');
+	        return;
+	    };
+
+
+	    var options =   {   quality: 50,
+	    					targetWidth: 720,
+	                        destinationType: Camera.DestinationType.DATA_URL,
+	                        sourceType: source,      // 0:Photo Library, 1=Camera, 2=Saved Album
+	                        encodingType: Camera.EncodingType.JPEG,
+	                        correctOrientation: true
+	                    };
+	    navigator.camera.getPicture(
+	        function(imageData){
+	        	$('.imgtitle').remove();
+	            $('.photozone', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+	        },
+	        function() {
+	            alert('Error taking picture', 'Error');
+	        },
+	        options);
+	};
+	
+	
+
+	
+//-----------------------------------------------------
+	init=function(){
+		console.log('Ready for test run')
+	}
+	
+//-----------------------------------------------------
+	document.addEventListener('deviceready', onDeviceReady, false);
+	onDeviceReady=function(){
+	    init();
+	}
